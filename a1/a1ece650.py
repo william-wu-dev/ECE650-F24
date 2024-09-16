6257d7e2516a0f55aa9756584aad529832430e72
Michael <yulin.wu297@outlook.com>
2024-09-16 18:05:43 -0400

    CONNECT LINE PARSER AND STREET DATABASE TO THE CENTER
    connect center with parser and database

--- a1/a1ece650.py
+++ a1/a1ece650.py
@@ -2,11 +2,12 @@
 import sys
 
 # YOUR CODE GOES HERE
-
+from line_parser import line_parser
+from StreetDatabase import StreetDatabase
 
 def main():
     # YOUR MAIN CODE GOES HERE
-
+    sdb = StreetDatabase()  # instantiate street database
     # sample code to read from stdin.
     # make sure to remove all spurious print statements as required
     # by the assignment
@@ -14,9 +15,27 @@ def main():
         line = sys.stdin.readline()
         if line == "":
             break
-        print("read a line:", line)
+        # print('READ LINE:', line, file=sys.stdout)
+        try:
+            # parse command
+            command, street_name, coordinates_list = line_parser(line)
 
-    print("Finished reading input")
+            # switch action based on command like a compiler
+            if command == 'add':
+                sdb.add(street_name, coordinates_list)
+            elif command == 'mod':
+                sdb.mod(street_name, coordinates_list)
+            elif command == 'rm':
+                sdb.rm(street_name)
+            elif command == 'gg':
+                # TODO: graph generator. Now we just print database for testing
+                print(str(sdb), file=sys.stdout)
+            else:
+                raise Exception(f'unknown command parsed `{command}\'')
+        except Exception as e:
+            print('Error:', str(e), file=sys.stderr)
+
+    # print("Finished reading input")
     # return exit code 0 on successful termination
     sys.exit(0)
 
