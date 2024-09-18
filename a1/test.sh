823b0963663d7c75db7539837ea2da4f7e6bf780
Michael <yulin.wu297@outlook.com>
2024-09-18 14:37:30 -0400

    ADD MOCK TEST CASE 1
    test non-existing command

--- a1/test.sh
+++ a1/test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
 python3 a1ece650.py < test_in.txt > test_out.txt 2>test_err.txt
+python3 a1ece650.py < ./test_in/test_in_1.txt > ./test_out/test_out_1.txt 2>./test_err/test_err_1.txt
