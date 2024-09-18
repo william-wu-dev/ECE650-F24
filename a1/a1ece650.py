6616a6074383e76d07347ba80edbdaa7e3310425
Michael <yulin.wu297@outlook.com>
2024-09-18 11:59:41 -0400

    CONNECT GRAPH GENERATOR TO CENTRE

--- a1/a1ece650.py
+++ a1/a1ece650.py
@@ -4,6 +4,7 @@ import sys
 # YOUR CODE GOES HERE
 from line_parser import line_parser
 from StreetDatabase import StreetDatabase
+from graph_generator import graph_generator
 
 def main():
     # YOUR MAIN CODE GOES HERE
@@ -29,7 +30,9 @@ def main():
                 sdb.rm(street_name)
             elif command == 'gg':
                 # TODO: graph generator. Now we just print database for testing
-                print(str(sdb), file=sys.stdout)
+                # print(str(sdb), file=sys.stdout)
+                graph = graph_generator(sdb)
+                print(str(graph), file=sys.stdout)
             else:
                 raise Exception(f'unknown command parsed `{command}\'')
         except Exception as e:
