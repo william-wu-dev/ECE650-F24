70bcc6608d914dcd82c75785f12df084a82903cf
Michael <yulin.wu297@outlook.com>
2024-09-18 11:59:28 -0400

    IMPLEMENT GRAPH GENERATOR

--- a1/graph_generator.py
+++ a1/graph_generator.py
@@ -8,41 +8,177 @@ email: y297wu@uwaterloo.ca
 from Graph import Graph
 from StreetDatabase import StreetDatabase
 
+import copy
+
 def graph_generator(sdb: StreetDatabase) -> Graph:
     # initialization
     # set of intersection
     # dictionary of added intersection street segment
     # result
-    intersections = set()
-    intermediate_sdb = dict()
+    intersections_set = set()
+    intermediate_sdb = copy.deepcopy(sdb._streetDatabase)
     res = Graph()
 
     # double pointer iterate sdb find intersection
+    street_list = list(sdb._streetDatabase.keys())
+    for i in range(0, len(street_list) - 1):
+        for j in range(i + 1, len(street_list)):
+            street_1 = street_list[i]
+            street_2 = street_list[j]
+            # count the intersection for every segment in 1 X every segment in 2
+            coordinates_list_1 = sdb._streetDatabase[street_1]
+            coordinates_list_2 = sdb._streetDatabase[street_2]
+            # iterate every segment in 1 and every segment in 2
+            for m in range(0, len(coordinates_list_1) - 1):
+                for n in range(0, len(coordinates_list_2) - 1):
+                    p1 = Point(coordinates_list_1[m])
+                    q1 = Point(coordinates_list_1[m + 1])
+                    p2 = Point(coordinates_list_2[n])
+                    q2 = Point(coordinates_list_2[n + 1])
+
+                    # check if <p1, q1> and <p2, q2> intersect and add intersection and add these 4 end-points to the result
+
+                    check_intersect_res = check_intersect(p1, q1, p2, q2)
+
+                    if check_intersect_res == 1:
+                        # intersect regularly, 
+                        # 1. find the only intersect and 
+                        # 2. add intersection to set intersections_set and 
+                        # 3. add intersection and 4 endpoints to res and 
+                        # 4. add intersection to intermediate db to break the line segment
+                        intersection = intersect(p1, q1, p2, q2)
+
+                        intersections_set.add(intersection.to_tuple())
+
+                        res.add_vertex(intersection.to_tuple())
+                        res.add_vertex(p1.to_tuple())
+                        res.add_vertex(q1.to_tuple())
+                        res.add_vertex(p2.to_tuple())
+                        res.add_vertex(q2.to_tuple())
+
+                        # add intersection to break line segment <p1, q1> in intermediate_sdb
+                        # 1. determine the break position
+                        # 2. insert 
+                        intermediate_coordinates_list_1 = intermediate_sdb[street_1]
+                        lo = intermediate_coordinates_list_1.index(p1.to_tuple())
+                        hi = intermediate_coordinates_list_1.index(q1.to_tuple())
+                        i = lo + 1
+                        while dist(Point(intermediate_coordinates_list_1[lo]), Point(intermediate_coordinates_list_1[i])) <= \
+                            dist(Point(intermediate_coordinates_list_1[lo]), intersection) and  i < hi:
+                            i += 1
+                        intermediate_coordinates_list_1.insert(i, intersection.to_tuple())
+
+                        # the same for the other line segment
+                        intermediate_coordinates_list_2 = intermediate_sdb[street_2]
+                        lo = intermediate_coordinates_list_2.index(p2.to_tuple())
+                        hi = intermediate_coordinates_list_2.index(q2.to_tuple())
+                        i = lo + 1
+                        while dist(Point(intermediate_coordinates_list_2[lo]), Point(intermediate_coordinates_list_2[i])) <= \
+                            dist(Point(intermediate_coordinates_list_2[lo]), intersection) and i < hi:
+                            i += 1
+                        intermediate_coordinates_list_2.insert(i, intersection.to_tuple())
+
+                    elif check_intersect_res == 0:
+                        # intersect by overlap, intersection number may vary as they might have only overlap a vertex.
+                        # so, for every intersect we must do the same as only 1 intersect
+                        intersections_list = overlap_intersect(p1, q1, p2, q2)
+
+                        for intersection in intersections_list:
+                            intersections_set.add(intersection.to_tuple())
+
+                        for intersection in intersections_list:
+                            res.add_vertex(intersection.to_tuple())
+
+                        res.add_vertex(p1.to_tuple())
+                        res.add_vertex(q1.to_tuple())
+                        res.add_vertex(p2.to_tuple())
+                        res.add_vertex(q2.to_tuple())
+
+                        # add intersection to break line segment <p1, q1> in intermediate_sdb
+                        # 1. determine the break position
+                        # 2. insert 
+                        for intersection in intersections_list:
+                            intermediate_coordinates_list_1 = intermediate_sdb[street_1]
+                            lo = intermediate_coordinates_list_1.index(p1.to_tuple())
+                            hi = intermediate_coordinates_list_1.index(q1.to_tuple())
+                            i = lo + 1
+                            while dist(Point(intermediate_coordinates_list_1[lo]), Point(intermediate_coordinates_list_1[i])) <= \
+                                dist(Point(intermediate_coordinates_list_1[lo]), intersection) and  i < hi:
+                                i += 1
+                            intermediate_coordinates_list_1.insert(i, intersection.to_tuple())
+
+                        # the same for the other line segment
+                        for intersection in intersections_list:
+                            intermediate_coordinates_list_2 = intermediate_sdb[street_2]
+                            lo = intermediate_coordinates_list_2.index(p2.to_tuple())
+                            hi = intermediate_coordinates_list_2.index(q2.to_tuple())
+                            i = lo + 1
+                            while dist(Point(intermediate_coordinates_list_2[lo]), Point(intermediate_coordinates_list_2[i])) <= \
+                                dist(Point(intermediate_coordinates_list_2[lo]), intersection) and i < hi:
+                                i += 1
+                            intermediate_coordinates_list_2.insert(i, intersection.to_tuple())
+
+    # based on intermediate database and intersections_set set
+    # add edge to res
+    for street in street_list:
+        intermediate_coordinates_list = intermediate_sdb[street]
+        for i in range(0, len(intermediate_coordinates_list)):
+            # i is an intersect add edge <left, i> and <i, right> to the edge only if
+            # left or right exist and 
+            # left or right is not i
+            if intermediate_coordinates_list[i] in intersections_set:
+                intersection = intermediate_coordinates_list[i]
+                intersection_id = res.get_vertex_id(intersection)
+                if i > 0 and i < len(intermediate_coordinates_list) - 1:
+                    left = intermediate_coordinates_list[i - 1]
+                    right = intermediate_coordinates_list[i + 1]
+                    if left != intersection:
+                        left_id = res.get_vertex_id(left)
+                        if left_id < intersection_id:
+                            res.add_edge(left_id, intersection_id)
+                        elif left_id > intersection_id:
+                            res.add_edge(intersection_id, left_id)
+                    if right != intersection:
+                        right_id = res.get_vertex_id(right)
+                        if intersection_id < right_id:
+                            res.add_edge(intersection_id, right_id)
+                        elif intersection_id > right_id:
+                            res.add_edge(right_id, intersection_id)
+                elif i == 0:  # no intersection left
+                    right = intermediate_coordinates_list[i + 1]
+                    if right != intersection:
+                        right_id = res.get_vertex_id(right)
+                        if intersection_id < right_id:
+                            res.add_edge(intersection_id, right_id)
+                        elif intersection_id > right_id:
+                            res.add_edge(right_id, intersection_id)
+                elif i == len(intermediate_coordinates_list) - 1:  # no intersection right
+                    left = intermediate_coordinates_list[i - 1]
+                    if left != intersection:
+                        left_id = res.get_vertex_id(left)
+                        if left_id < intersection_id:
+                            res.add_edge(left_id, intersection_id)
+                        elif left_id > intersection_id:
+                            res.add_edge(intersection_id, left_id)
 
     return res
 
 # The following are helpers
 
 class Point(object):
-    def __init__ (self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+    def __init__ (self, coordinate: tuple):
+        self.x = float(coordinate[0])
+        self.y = float(coordinate[1])
     def __str__ (self):
         return '(' + str(self.x) + ',' + str(self.y) + ')'
+    def to_tuple(self):
+        return (self.x, self.y)
 
-class Line(object):
-    def __init__ (self, src, dst):
-        self.src = src
-        self.dst = dst
-
-    def __str__(self):
-        return str(self.src) + '-->' + str(self.dst)
-
-def intersect (l1, l2):
-    x1, y1 = l1.src.x, l1.src.y
-    x2, y2 = l1.dst.x, l1.dst.y
-    x3, y3 = l2.src.x, l2.src.y
-    x4, y4 = l2.dst.x, l2.dst.y
+def intersect (p1: Point, q1: Point, p2: Point, q2: Point):
+    x1, y1 = p1.x, p1.y
+    x2, y2 = q1.x, q1.y
+    x3, y3 = p2.x, p2.y
+    x4, y4 = q2.x, q2.y
 
     xnum = ((x1*y2-y1*x2)*(x3-x4) - (x1-x2)*(x3*y4-y3*x4))
     xden = ((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4))
@@ -52,7 +188,7 @@ def intersect (l1, l2):
     yden = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
     ycoor = ynum / yden
 
-    return Point (xcoor, ycoor)
+    return Point ((xcoor, ycoor))
 
 def overlap_intersect(p1: Point, q1: Point, p2: Point, q2: Point):
     if max(p1.x, q1.x) >= max(p2.x, q2.x) and min(p1.x, q1.x) <= min(p2.x, q2.x):
@@ -154,5 +290,8 @@ def check_intersect(p1: Point, q1: Point, p2: Point, q2: Point) -> int:
     
     return -1  # all other situations are not intersect
 
-
+def dist(a: Point, b:Point):
+    x1, y1 = a.x, a.y
+    x2, y2 = b.x, b.y
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
 
