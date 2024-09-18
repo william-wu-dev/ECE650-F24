8ed4849519c1c29457abb5d2dd08a50b209191e2
Michael <yulin.wu297@outlook.com>
2024-09-18 16:23:57 -0400

    UPGRADE TEST SCRIPT AND ADD MORE TEST CASES

--- a1/test.sh
+++ a1/test.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 
 python3 a1ece650.py < test_in.txt > test_out.txt 2>test_err.txt
-python3 a1ece650.py < ./test_in/test_in_1.txt > ./test_out/test_out_1.txt 2>./test_err/test_err_1.txt
+
+TEST_CASE_COUNT=9
+readonly TEST_CASE_COUNT
+
+for ((i=1; i<=TEST_CASE_COUNT; i++))
+do
+    python3 a1ece650.py < "./test_in/test_in_${i}.txt" > "./test_out/test_out_${i}.txt" 2>"./test_err/test_err_${i}.txt"
+done
