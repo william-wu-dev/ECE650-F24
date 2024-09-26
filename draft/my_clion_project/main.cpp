3755d4b46284d9dbcb1d3d84d00fdbf6b70398bd
Michael <yulin.wu297@outlook.com>
2024-09-26 17:36:48 -0400

    TEST EOF FUNCTION AND BASIC THROW-CATCH

--- draft/my_clion_project/main.cpp
+++ draft/my_clion_project/main.cpp
@@ -4,9 +4,35 @@ using namespace std;
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << endl;
-    cerr << "some error" << endl;
+    while (!cin.eof())
+    {
+        string line;
+        getline(cin, line);
+
+        if (line.empty())
+        {
+            continue;
+        }
+
+        // try
+        // {
+        //     throw "some error 123456";
+        // }
+        // catch (const char *error_msg)
+        // {
+        //     cerr << error_msg << endl;
+        // }
+
+        try
+        {
+            throw string("some error 123456");
+        }
+        catch (string error_msg)
+        {
+            cerr << error_msg << endl;
+        }
+
+        cout << line << endl;
+    }
     return 0;
 }
