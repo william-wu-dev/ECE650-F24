411d2489ca2d274da7aa586786a7bd85d898869c
Michael <yulin.wu297@outlook.com>
2024-09-27 21:18:04 -0400

    CONFIGURE AND TEST RUN SCRIPT

--- a2/run_local.sh
+++ a2/run_local.sh
@@ -1,14 +1,3 @@
-# check if exist
-# FIXME: comment out if this is on linux
-if [ ! -d "/Volumes/macOS/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/bin/" ]; then
-    printf "/Volumes/macOS/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/bin/ no such directory, please plug in the external disk\n"
-    exit 1
-fi
-
-# prepare for cmake
-# FIXME: comment out if this is on linux
-export PATH="/Volumes/macOS/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/bin/:${PATH}"
-
 # FIXME: change this to fit your project
 PATH_TO_SOURCE="./"
 PATH_TO_BUILD="./cmake-build-debug"
@@ -20,27 +9,27 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-if [ $1 -eq 0 ]; then
+if [ "$1" -eq 0 ]; then
     printf "starting cmake \n"
     cmake -DCMAKE_BUILD_TYPE=Debug -S ${PATH_TO_SOURCE} -B ${PATH_TO_BUILD}
-elif [ $1 -eq 1 ]; then
+elif [ "$1" -eq 1 ]; then
     printf "starting cmake \n"
     cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S ${PATH_TO_SOURCE} -B ${PATH_TO_BUILD}
-elif [ $1 -eq 2 ]; then
+elif [ "$1" -eq 2 ]; then
     printf "starting cmake \n"
     cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DWITH_ASAN=ON -S ${PATH_TO_SOURCE} -B ${PATH_TO_BUILD}
 else
-    printf "unknown argument \"$1\", 0 for plain cmake, 1 for clang++ compile, 2 for address sanination activation.\n"
+    printf "unknown argument \"%s\", 0 for plain cmake, 1 for clang++ compile, 2 for address sanitation activation.\n" "$1"
     exit 1
 fi
 
 # make in a directory
-printf "\nstarting make\n"
+printf "\n"
+printf "starting make\n"
 make -C ${PATH_TO_BUILD}
 
 # test
-# FIXME: change this to your target name
-TARGET_NAME="my_clion_project"
+TARGET_NAME="ece650-a2"
 
 if [ ! -d "./test_out" ]; then
     mkdir ./test_out
@@ -51,12 +40,13 @@ if [ ! -d "./test_err" ]; then
 fi
 
 # FIXME: change this to fit your test case count
-TEST_CASE_COUNT=3
+TEST_CASE_COUNT=4
 readonly TEST_CASE_COUNT
 
-printf "\ntarget ${TARGET_NAME} test started, ${TEST_CASE_COUNT} test case in total\n"
+printf "\n"
+printf "target %s test started, %d test case in total\n" "${TARGET_NAME}" ${TEST_CASE_COUNT}
 
 for ((i = 1; i <= TEST_CASE_COUNT; i++)); do
-    printf "starting test case #${i}\n"
+    printf "starting test case #%d\n" ${i}
     "${PATH_TO_BUILD}/${TARGET_NAME}" <"./test_in/test_in_${i}.txt" >"./test_out/test_out_${i}.txt" 2>"./test_err/test_err_${i}.txt"
 done
