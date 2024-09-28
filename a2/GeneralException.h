9553c16f08005361f5cbe08d2da71be108534a9f
Michael <yulin.wu297@outlook.com>
2024-09-28 19:11:07 -0400

    VIRTUAL FUNCTION IMPLEMENTATION MOVED INTO HEADER FILE

---
+++ a2/GeneralException.h
@@ -0,0 +1,26 @@
+//
+// Created by Michael Williams on 2024-09-28.
+//
+
+#ifndef GENERALEXCEPTION_H
+#define GENERALEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+namespace a2 {
+    class GeneralException : public std::exception {
+    private:
+        std::string message;
+
+    public:
+        GeneralException(std::string message) : message(message) {
+        };
+
+        virtual const char *what() const throw() {
+            return message.c_str();
+        }
+    };
+} // a2
+
+#endif //GENERALEXCEPTION_H
