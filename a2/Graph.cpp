cc88da64f9ad183351fda4365da829d0d4f58946
Michael <yulin.wu297@outlook.com>
2024-09-28 19:11:23 -0400

    COMPLETE GRAPH CLASS

---
+++ a2/Graph.cpp
@@ -0,0 +1,116 @@
+//
+// Created by Michael Williams on 2024-09-28.
+//
+
+#include "Graph.h"
+#include "GeneralException.h"
+#include <string>
+#include <algorithm>
+
+namespace a2 {
+    Graph::Graph(int vertexCount) : vertexCount(vertexCount) {
+    }
+
+    void Graph::addEdge(int from, int to, int weight) noexcept(false) {
+        // TODO: check exceed-limit from to and from == to
+        // check vertex validity
+        if (from > vertexCount) {
+            std::string message = "vertex ";
+            message += std::to_string(from);
+            message += " of edge <";
+            message += std::to_string(from);
+            message += ", ";
+            message += std::to_string(to);
+            message += "> is not defined.";
+            throw GeneralException(message);
+        }
+        if (to > vertexCount) {
+            std::string message = "vertex ";
+            message += std::to_string(to);
+            message += " of edge <";
+            message += std::to_string(from);
+            message += ", ";
+            message += std::to_string(to);
+            message += "> is not defined.";
+            throw GeneralException(message);
+
+        }
+        if (from == to) {
+            std::string message = "edge <";
+            message += std::to_string(from);
+            message += ", ";
+            message += std::to_string(to);
+            message += "> incident on the same vertex is not allowed.";
+            throw GeneralException(message);
+        }
+        // check duplicated edges
+        for (auto & edge : edges) {
+            if (edge.from == from && edge.to == to) {
+                std::string message = "edge <";
+                message += std::to_string(from);
+                message += ", ";
+                message += std::to_string(to);
+                message += "> already added.";
+                throw GeneralException(message);
+            }
+        }
+        // add edges
+        edges.push_back({from, to, weight});
+        edges.push_back({to, from, weight}); // undirected graph should add both opposite-directed edges
+    }
+
+    std::string Graph::compute(int from, int to) noexcept(false) {
+        // initialization
+        std::vector<int> distances(vertexCount + 1, 0x3f3f3f3f);
+        // vertex number starts from 1, initialize distance to inf
+        std::vector<int> parents(vertexCount + 1, -1); // initialize parent to -1
+        distances[from] = 0;
+        // iterate for |V| - 1 times
+        bool flag = true; // early stop flag
+        for (int i = 1; i <= vertexCount - 1 && flag; i++) {
+            flag = false; // if the distance will not change in this iteration, halt
+            for (auto edge: edges) {
+                // relax job
+                int u = edge.from;
+                int v = edge.to;
+                int weight = edge.weight;
+                if (distances[v] > distances[u] + weight) {
+                    distances[v] = distances[u] + weight;
+                    parents[v] = u; // if relaxed, mark the parent
+                    flag = true; // the distance indeed changes
+                }
+            }
+        }
+
+        // check if "to" is reachable from "from"
+        if (distances[to] > 0x3f3f3f3f / 2) {
+            // "to" cannot be reached from "from" throw exception
+            std::string message;
+            message += std::to_string(to);
+            message += " is not reachable from ";
+            message += std::to_string(from);
+            message += ".";
+            throw GeneralException(message);
+        }
+
+        // generate path from "from" to "to"
+        std::vector<int> path;
+        path.push_back(to);
+        int parent = parents[to];
+        while (parent != -1) {
+            path.push_back(parent);
+            parent = parents[parent];
+        }
+        // reverse this path
+        std::reverse(path.begin(), path.end());
+        // print out the path
+        std::string result;
+        for (int i = 0; i < path.size(); i++) {
+            if (i) {
+                result += "-";
+            }
+            result += std::to_string(path[i]);
+        }
+        return result;
+    }
+} // a2
