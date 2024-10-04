91da4d4af0a9fd6f0a5ff3e80833b006431d0c61
Michael <yulin.wu297@outlook.com>
2024-10-04 17:37:34 -0400

    UPDATE CODING STYLE CHECK SCRIPT BASED ON POST 129
    *.cpp

--- a2/coding_style.sh
+++ a2/coding_style.sh
@@ -3,6 +3,6 @@ PATH_TO_CMAKE_SOURCE="./"
 PATH_TO_CMAKE_BUILD="./cmake-build-clang-tidy"
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S ${PATH_TO_CMAKE_SOURCE} -B ${PATH_TO_CMAKE_BUILD}
-python3 run-clang-tidy.py -p cmake-build-clang-tidy ece650-a2.cpp
+python3 run-clang-tidy.py -p cmake-build-clang-tidy *.cpp
 # python3 run-clang-tidy.py -p cmake-build-clang-tidy Graph.cpp
 # python3 run-clang-tidy.py -p cmake-build-clang-tidy test_graph.cpp
