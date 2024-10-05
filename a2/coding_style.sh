d99e0bb2bb6dd218fab2bd63b8f7bef41c597792
Michael <yulin.wu297@outlook.com>
2024-10-05 11:27:59 -0400

    UPDATE CODING STYLE CHECK BASED ON POST 152
    clang-tidy -p build ece650-a2.cpp

--- a2/coding_style.sh
+++ a2/coding_style.sh
@@ -1,8 +1,18 @@
 #! /usr/bin/bash
+
+CLEAR_BUILD_FOLDER=true
+
 PATH_TO_CMAKE_SOURCE="./"
 PATH_TO_CMAKE_BUILD="./cmake-build-clang-tidy"
 
+# clear cmake build path
+if ${CLEAR_BUILD_FOLDER}; then
+    echo "starting to clear cmake build file"
+    printf "\n"
+    rm -rf ${PATH_TO_CMAKE_BUILD}/*
+fi
+
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S ${PATH_TO_CMAKE_SOURCE} -B ${PATH_TO_CMAKE_BUILD}
-python3 run-clang-tidy.py -p cmake-build-clang-tidy *.cpp
+clang-tidy -p ${PATH_TO_CMAKE_BUILD} *.cpp
 # python3 run-clang-tidy.py -p cmake-build-clang-tidy Graph.cpp
 # python3 run-clang-tidy.py -p cmake-build-clang-tidy test_graph.cpp
