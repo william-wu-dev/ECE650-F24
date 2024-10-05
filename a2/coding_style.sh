0850291530de1b3203f07419d6fb809edd099fad
Michael <yulin.wu297@outlook.com>
2024-10-05 12:29:32 -0400

    ADD MESSAGE BEFORE STARTING CLANG TIDY

--- a2/coding_style.sh
+++ a2/coding_style.sh
@@ -13,6 +13,9 @@ if ${CLEAR_BUILD_FOLDER}; then
 fi
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S ${PATH_TO_CMAKE_SOURCE} -B ${PATH_TO_CMAKE_BUILD}
+
+printf "\n"
+printf "starting clang-tidy\n"
 clang-tidy -p ${PATH_TO_CMAKE_BUILD} *.cpp
 # python3 run-clang-tidy.py -p cmake-build-clang-tidy Graph.cpp
 # python3 run-clang-tidy.py -p cmake-build-clang-tidy test_graph.cpp
