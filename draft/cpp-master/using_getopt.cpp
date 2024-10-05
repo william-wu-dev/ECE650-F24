c73cf7e00e6554f8a8a7d3da1f4993fb67791505
Yulin Wu <yulin.wu297@outlook.com>
2024-10-05 18:10:48 -0400

    LEARN GET OPT

--- draft/cpp-master/using_getopt.cpp
+++ draft/cpp-master/using_getopt.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <unistd.h>
 
+#define OLD_APPROACH false
+
 /// main() must be declared with arguments
 /// otherwise command line arguments are ignored
 int main(int argc, char **argv) {
@@ -10,13 +12,17 @@ int main(int argc, char **argv) {
   bool bflag = 0;
   std::string cvalue;
   int cint_value;
+  std::string dvalue;
+  int dint_value;
   int index;
   int c;
 
-  opterr = 0;
+  opterr = 0;  
+  // If getopt() does not recognize an option character, it prints an error message to stderr, stores the character in optopt, and returns '?'. The calling program may prevent the error message by setting opterr to 0.
 
+  // Generally, the getopt() function is called from inside of a loop’s conditional statement. The loop terminates when the getopt() function returns -1. A switch statement is then executed with the value returned by getopt() function. 
   // expected options are '-a', '-b', and '-c value'
-  while ((c = getopt(argc, argv, "abc:")) != -1)
+  while ((c = getopt(argc, argv, ":abc:d:")) != -1)  // ‘-1’ if there are no more options to process.
     switch (c) {
     case 'a':
       aflag = true;
@@ -25,15 +31,30 @@ int main(int argc, char **argv) {
       bflag = true;
       break;
     case 'c':
-      cvalue = optarg;
+      cvalue = optarg;  // If the option takes a value, that value is pointer to the external variable optarg
       cint_value = atoi(cvalue.c_str());
+      /*The C stdlib library atoi() function is used to convert a numeric string into an integer value.*/
       break;
-    case '?':
-      if (optopt == 'c')
-        std::cerr << "Error: option -" << optopt << " requires an argument."
-                  << std::endl;
+    case 'd':
+      dvalue = optarg;  // If the option takes a value, that value is pointer to the external variable optarg
+      dint_value = atoi(dvalue.c_str());
+      /*The C stdlib library atoi() function is used to convert a numeric string into an integer value.*/
+      break;
+    case ':':
+    /*
+    this case will handle no argument error as if (optopt == 'c') would do
+    */
+      std::cerr << "Error: option -" << (char)optopt << " requires an argument. By colon"
+                  << std::endl;  // you need to chage optopt to char 
+      break;
+    case '?':  // return ‘?’ when there is an unrecognized option and it stores into external variable optopt.
+      #if OLD_APPROACH
+      if (optopt == 'c')  // If an option requires a value (such as -f in our example) and no value is given, getopt normally returns ?
+        std::cerr << "Error: option -" << (char)optopt << " requires an argument."
+                  << std::endl;  // you need to chage optopt to char 
       else
-        std::cerr << "Error: unknown option: " << optopt << std::endl;
+      #endif
+        std::cerr << "Error: unknown option: " << (char)optopt << std::endl;
       return 1;
     default:
       return 0;
@@ -42,8 +63,12 @@ int main(int argc, char **argv) {
   std::cout << "aflag=" << aflag << " "
             << "bflag=" << bflag << " "
             << "cvalue=" << cvalue << " "
-            << "cint_value=" << cint_value << std::endl;
+            << "cint_value=" << cint_value << " "
+            << "dvalue=" << dvalue << " "
+            << "dint_value=" << dint_value << std::endl;
 
+  // optind is for the extra arguments 
+  // which are not parsed 
   if (optind < argc) {
     std::cout << "Found positional arguments\n";
     for (index = optind; index < argc; index++)
