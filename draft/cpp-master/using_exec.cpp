aad3d827cb57fb9175b77d54880faed668e5191c
Yulin Wu <yulin.wu297@outlook.com>
2024-10-05 17:27:51 -0400

    LEARN EXEC NOTES

--- draft/cpp-master/using_exec.cpp
+++ draft/cpp-master/using_exec.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <cstring>
+#include <cerrno>
 
 int main(void) {
 
@@ -9,7 +11,8 @@ int main(void) {
 
   argv[0] = (char *)"ls";
   argv[1] = (char *)"-l";
-  argv[2] = nullptr;
+  // argv[2] = nullptr;
+  argv[2] = (char *)"not_exist";
 
   // std::endl print line terminator and flushes output stream!
   std::cout << "[exec] executing '/bin/ls -l' using execv" << std::endl;
@@ -21,14 +24,39 @@ int main(void) {
     sleep(4);
     execv("/bin/ls", argv);
     // execl("/bin/ls", "ls", "-l", nullptr);
-    perror("Error from arie");
+    perror("Error from arie");  // the equivalent is in the follow
+    // Clipped from: https://www.ibm.com/docs/en/i/7.3?topic=functions-perror-print-error-message
+    // perror() — Print Error Message
+    // Defined in header <cstdio>
+    // The message associated with the value in errno is then printed followed by a new-line character.
+    std::cerr << "Error Message by strerror: " << strerror(errno) << std::endl;
+    // strerror in <cstring> and errno in <cerrno>
+    // Returns a pointer to the textual description of the system error code errnum, identical to the description that would be printed by std::perror().
+    // Clipped from: https://en.cppreference.com/w/cpp/string/byte/strerror
     return 1;
   }
 
   int res;
   std::cout << "[exec] waiting for child process to terminate" << std::endl;
   waitpid(kid, &res, 0);
-  std::cout << "[exec] finished execution with return status: " << res
+
+  /*
+  res will be set to 0 i.e., ls return code if ls ended correctly
+  res will be set to some other number if ls ended in error and kid return 1
+  #include <sys/types.h>
+  #include <sys/wait.h>
+
+  WIFEXITED(status)
+  returns true if the child terminated normally, that is, by calling exit(3) or _exit(2), or by returning from main().
+  WEXITSTATUS(status)
+  returns the exit status of the child. This consists of the least significant 8 bits of the status argument that the child specified in a call to exit(3) or _exit(2) or as the argument for a return statement in main(). This macro should only be employed if WIFEXITED returned true.
+
+  Clipped from: https://linux.die.net/man/2/waitpid
+  */
+
+  std::cout << "[exec] finished execution with return status[Non-readable]: " << res
+            << std::endl;
+  std::cout << "[exec] finished execution with return status: " << WEXITSTATUS(res)
             << std::endl;
 
   return 0;
