c43ebf203d90f95f316d9de4149a7ab4d814b6d0
Michael <yulin.wu297@outlook.com>
2024-10-21 16:49:09 -0400

    WHEN RGEN RECEIVES INVALID ARGUMENTS, TERMINATE
    rgen should terminate if passed an invalid argument (e.g., -s 1). In this case, generating any further output would be meaningless.
    
    @200

--- a3/rgen.cpp
+++ a3/rgen.cpp
@@ -158,6 +158,7 @@ int main(int argc, char **argv) {
 #endif
     } catch (std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
     try {
